67ce3e8f4904b443c0650d55b9c903bf20a9d987
federicos1 <fedes7777@gmail>
2022-08-10 10:21:48 -0300

    ejercicio 1 valoracion final

---
+++ ejercicio.py
@@ -0,0 +1,18 @@
+"""
+1) Crear una lista en Python denominada “Dueno” que contenga los siguientes valores:
+
+    28957346,  Juan,  Perez, 4789689,  Belgrano 101
+
+  Dichos datos se corresponden  con los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección). Muestre en pantalla el teléfono del dueño si el DNI es mayor a 26000000.
+
+"""
+
+
+
+dueno = [28957346,'Juan', 4789689, 'Belgrano 101' ]
+dni = dueno[0]
+
+if dni > 26000000:
+    print(f'Telefono del dueño: {dueno[2]}')
+
+
